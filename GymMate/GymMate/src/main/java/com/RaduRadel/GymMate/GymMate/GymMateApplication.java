d546a78ce92fe54cf86f54c6ba265f5c0d1d59a4
MadalinaEliana <madalina.stefan2013@gmail.com>
2025-05-05 15:43:16 +0300

    initial SpringBoot skeleton

---
+++ GymMate/GymMate/src/main/java/com/RaduRadel/GymMate/GymMate/GymMateApplication.java
@@ -0,0 +1,13 @@
+package com.RaduRadel.GymMate.GymMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymMateApplication.class, args);
+	}
+
+}
